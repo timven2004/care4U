45809e4fdf7c79aeb9aff27b0c120297691445e5
Tim Leung <timven2004@gmail.com>
2021-02-25 15:49:48 +0800

    created Payment pages

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ const app = express()
 
 app.use(express.static('public'));
 
+app.use((req,res)=>{
+    res.status(404).json({message:"404 Not found"})
+})
 
 app.listen(8080, ()=>{
     console.log("server started! listening on port 8080")
