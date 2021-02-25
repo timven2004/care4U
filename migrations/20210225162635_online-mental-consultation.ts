1a6011e585e5ee1d3288fbb640523541bda6f8e9
Tim Leung <timven2004@gmail.com>
2021-02-25 17:19:42 +0800

    updated migration

--- migrations/20210225162635_online-mental-consultation.ts
+++ migrations/20210225162635_online-mental-consultation.ts
@@ -2,7 +2,7 @@ import * as Knex from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable("Users", (table)=>{
+    await knex.schema.createTable("Users", (table) => {
         table.increments("id");
         table.string("name");
         table.string("email");
@@ -12,7 +12,7 @@ export async function up(knex: Knex): Promise<void> {
         table.date("updated_at")
     })
 
-    await knex.schema.createTable("Doctors", (table)=>{
+    await knex.schema.createTable("Doctors", (table) => {
         table.increments("id");
         table.string("name");
         table.string("email");
@@ -24,7 +24,7 @@ export async function up(knex: Knex): Promise<void> {
 
     })
 
-    await knex.schema.createTable("Questionnaires", (table)=>{
+    await knex.schema.createTable("Questionnaires", (table) => {
         table.increments("id");
         table.integer("user_id");
         table.foreign("user_id").references("Users.id");
@@ -34,7 +34,7 @@ export async function up(knex: Knex): Promise<void> {
         table.integer("other_symptoms")
     })
 
-    await knex.schema.createTable("Bookings", (table)=>{
+    await knex.schema.createTable("Bookings", (table) => {
         table.increments("id");
         table.timestamp("time");
         table.integer("user_id");
@@ -44,17 +44,17 @@ export async function up(knex: Knex): Promise<void> {
         table.integer("questionnaire_id");
         table.foreign("questionnaire_id").references("Questionnaires.id");
         table.boolean("is_active");
-    }) 
+    })
 
-    await knex.schema.createTable("Doctors_available_time_slots", (table)=>{
+    await knex.schema.createTable("Doctors_available_time_slots", (table) => {
         table.increments("id");
         table.integer("doctor_id");
         table.foreign("doctor_id").references("Doctors.id");
         table.timestamp("time_start");
         table.timestamp("time_end")
-    }) 
+    })
 
-    await knex.schema.createTable("Transaction_records", (table)=>{
+    await knex.schema.createTable("Transaction_records", (table) => {
         table.increments("id");
         table.integer("consultation_fee");
         table.integer("service_fee");
@@ -66,23 +66,24 @@ export async function up(knex: Knex): Promise<void> {
         table.foreign("booking_id").references("Bookings.id")
         table.boolean("is_success")
         table.timestamp("payment_date")
+    })
 
-        await knex.schema.createTable("Medical_records", (table)=>{
-            table.increments("id");
-            table.text("doctor_comment");
-            table.integer("user_id");
-            table.foreign("user_id").references("Users.id")
-            table.integer("doctor_id")
-            table.foreign("doctor_id").references("Doctors.id")
-            table.integer("booking_id")
-            table.foreign("booking_id").references("Bookings.id")
-            table.integer("transaction_id")
-            table.foreign("transaction_id").references("Transaction_records.id")
-    
+    await knex.schema.createTable("Medical_records", (table) => {
+        table.increments("id");
+        table.text("doctor_comment");
+        table.integer("user_id");
+        table.foreign("user_id").references("Users.id")
+        table.integer("doctor_id")
+        table.foreign("doctor_id").references("Doctors.id")
+        table.integer("booking_id")
+        table.foreign("booking_id").references("Bookings.id")
+        table.integer("transaction_id")
+        table.foreign("transaction_id").references("Transaction_records.id")
 
 
-    }) 
-}}
+
+    })
+}
 
 
 export async function down(knex: Knex): Promise<void> {
