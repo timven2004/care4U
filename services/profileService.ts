3c075966e2d127808792c178d5066258fde26ef5
Tim Leung <timven2004@gmail.com>
2021-02-26 15:26:09 +0800

    updated userservice

--- services/profileService.ts
+++ services/profileService.ts
@@ -1,55 +1,87 @@
 import Knex from 'knex'
-import {Request} from "express"
+import { Request } from "express"
 
 export class ProfileService {
-    private knex:Knex
-    constructor (knex:Knex){
+    private knex: Knex
+    constructor(knex: Knex) {
         this.knex = knex
     }
 
-    async getUserProfile(id:number, withPassword:boolean = false){
-        const result = await this.knex.select("name","email","telephone","password","created_at","updated_at").from("users").where("id",id)
-        if (withPassword){
-            return result[0]}
-        else {
-            result[0].password = "hidden from server";
-            return result
+    async getUserProfile(id: number, withPassword: boolean = false) {
+        const result = await this.knex.select("name", "email", "telephone", "password", "created_at", "updated_at").from("users").where("id", id)
+
+        if (result[0]) {
+            if (withPassword) {
+                return result[0]
+            }
+            else {
+                result[0].password = "hidden from server";
+                return result
+            }
         }
-        
+
+        throw "User Not Found from profileServices";
     }
 
-    async updateUserProfile(id:number, body:Request['body']){
-        await this.knex("users")
-                    .where("id",id)
-                    .update({"name":body["name"],
-                    "email":body["email"],
-                    "telephone":body["telephone"],
-                    "password":body["password"],
-                    "updated_at":this.knex.fn.now()})
+async updateUserProfile(id: number, body: Request['body']){
+
+    await this.knex.transaction(async (trx) => {
+        trx("users")
+            .where("id", id)
+            .update({
+                "name": body["name"],
+                "email": body["email"],
+                "telephone": body["telephone"],
+                "password": body["password"],
+                "updated_at": this.knex.fn.now()
+            })
+    })
+
+    // await this.knex("users")
+    //             .where("id",id)
+    //             .update({"name":body["name"],
+    //             "email":body["email"],
+    //             "telephone":body["telephone"],
+    //             "password":body["password"],
+    //             "updated_at":this.knex.fn.now()})
+}
+
+async getDoctorProfile(id: number, withPassword: boolean = false){
+    const result = await this.knex.select("name", "email", "telephone", "password", "created_at", "updated_at", "description").from("doctors").where("id", id)
+    if (withPassword) {
+        return result[0]
+    }
+    else {
+        result[0].password = "hidden from server";
+        return result
     }
 
-    async getDoctorProfile(id:number, withPassword:boolean = false){
-        const result = await this.knex.select("name","email","telephone","password","created_at","updated_at","description").from("doctors").where("id",id)
-        if (withPassword){
-            return result[0]}
-        else {
-            result[0].password = "hidden from server";
-            return result
-        }
-        
 
-    }
+}
 
-    async updateDoctorProfile(id:number, body:Request['body']){
-        await this.knex("users")
-                    .where("id",id)
-                    .update({"name":body["name"],
-                    "email":body["email"],
-                    "telephone":body["telephone"],
-                    "password":body["password"],
-                    "description":body["description"],
-                    "updated_at":this.knex.fn.now()})
-    }
+async updateDoctorProfile(id: number, body: Request['body']){
+
+    await this.knex.transaction(async (trx) => {
+        trx("doctors")
+            .where("id", id)
+            .update({
+                "name": body["name"],
+                "email": body["email"],
+                "telephone": body["telephone"],
+                "password": body["password"],
+                "updated_at": this.knex.fn.now()
+            })
+    })
+
+    // await this.knex("users")
+    //             .where("id",id)
+    //             .update({"name":body["name"],
+    //             "email":body["email"],
+    //             "telephone":body["telephone"],
+    //             "password":body["password"],
+    //             "description":body["description"],
+    //             "updated_at":this.knex.fn.now()})
+}
 
     
 }
