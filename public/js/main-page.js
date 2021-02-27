62613fe4e7c0cbbd8d6b373435397d1a34819200
Heady Yeung <heady_yeung@hotmail.com>
2021-02-27 22:29:41 +0800

    doctor & user login form layout

---
+++ public/js/main-page.js
@@ -0,0 +1,55 @@
+const userRegForm = document.querySelector("#reg-form");
+const userLoginForm = document.querySelector("#login-form");
+const switchRegLoginBtn = document.querySelector("#reg-login-switch a");
+let fillingLoginForm = true;
+let isLoggedIn = false;
+
+
+
+function switchLoginRegForm() {
+    userLoginForm.style.display = "block";
+    userRegForm.style.display = "none";
+    switchRegLoginBtn.innerHTML = "免費註冊";
+
+    switchRegLoginBtn.addEventListener("click", () => {
+        if (!fillingLoginForm) {
+            userLoginForm.style.display = "block";
+            userRegForm.style.display = "none";
+            switchRegLoginBtn.innerHTML = "免費註冊";
+        } else {
+            userLoginForm.style.display = "none";
+            userRegForm.style.display = "block";
+            switchRegLoginBtn.innerHTML = "立即登入";
+        }
+        fillingLoginForm = !fillingLoginForm;
+    });
+}
+switchLoginRegForm();
+
+
+// Submit UX handling
+function finishedRegister() {
+    const regOrLoginForm = document.querySelector("#reg-or-login-form");
+    const formResponse = document.querySelector("#login-response.form-submit-response");
+
+    formResponse.style.display = "flex";
+    regOrLoginForm.style.display = "none";
+    switchRegLoginBtn.style.display = "none";
+
+    formResponse.addEventListener("click", () => {
+        formResponse.style.display = "none";
+        switchRegLoginBtn.style.display = "block";
+        regOrLoginForm.style.display = "block";
+        switchLoginRegForm()
+    })
+}
+
+
+// function check_pass() {
+//     if (document.getElementById('password').value ==
+//             document.getElementById('confirm_password').value) {
+//         document.getElementById('submit').disabled = false;
+//     } else {
+//         document.getElementById('submit').disabled = true;
+//     }
+// }
