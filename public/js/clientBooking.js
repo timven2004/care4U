64917cb39a2a1b74ca5aa7b37ad93ea851077dab
Tim Leung <timven2004@gmail.com>
2021-03-01 11:48:16 +0800

    booking form added

--- public/js/clientBooking.js
+++ public/js/clientBooking.js
@@ -13,12 +13,6 @@ const fetchingData = async (userId) => {
 const choosenTimeSlot = document.querySelector("#choosenTimeSlot")
 const choosenTimeSlotId = document.querySelector("#choosenTimeSlotId")
 
-const selectTime = async (timeSlot, timeSlotId) =>{
-    choosenTimeSlot.value = timeSlot;
-    choosenTimeSlotId.value = timeSlotId;
-}
-
-
 
 
 async function load() {
@@ -61,34 +55,42 @@ async function load() {
         const availableDatesArray = data.filter(obj => {
             return obj.time_start.slice(0, 10) == myCalendar.getSelected()
         })
-
+       
         console.log(availableDatesArray)
 
         let timeSlotsStr = availableDatesArray.map((obj) => {
             let startTime = new Date(obj.time_start)
             let endTime = new Date(obj.time_end)
             let id = obj.id
-            const singleDigit = [0,1,2,3,4,5,6,7,8,9]
+            const singleDigit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            let startTimeStr = `${startTime.getHours()}:${singleDigit.includes(startTime.getMinutes()) ? "0" + startTime.getMinutes() : startTime.getMinutes()}`;
+            let endTimeStr = `${endTime.getHours()}:${singleDigit.includes(endTime.getMinutes()) ? "0" + endTime.getMinutes() : endTime.getMinutes()}`
+            let wholeTimeStr = `${startTimeStr} - ${endTimeStr}`
             return `
-            <button class="availableTimeBtn btn btn-success" id="${id}">
-            <h2>${startTime.getHours()}:${singleDigit.includes(startTime.getMinutes())? "0" + startTime.getMinutes() : startTime.getMinutes() } - ${endTime.getHours()}:${singleDigit.includes(endTime.getMinutes())? "0" + endTime.getMinutes():endTime.getMinutes() }</h2>
+            <button class="availableTimeBtn btn btn-success" onclick=selectTime("${startTimeStr}","${endTimeStr}",${id})>
+            <h2>${wholeTimeStr}</h2>
             <div class="doctor">${obj.doctor_name}</div>
             </button>
             `})
 
         timeSlotsStr = timeSlotsStr.join("")
 
-        timeSlots.innerHTML = 
-        `<h1>${myCalendar.getSelected()}</h1>
+        timeSlots.innerHTML =
+            `<h1>${myCalendar.getSelected()}</h1>
  
         
         ${timeSlotsStr}
         `
-        
-        
+
+
 
     })
 
+}
+
+const selectTime = async (startTime, endTime, timeSlotId) => {
+    choosenTimeSlot.value = `${startTime} - ${endTime}`;
+    choosenTimeSlotId.value = timeSlotId;
 }
 
 load();
